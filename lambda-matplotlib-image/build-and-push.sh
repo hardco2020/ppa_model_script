91b9429d84b44adcf3a187ca7f84c447c02524e9
You Kai <kaiyour@amazon.com>
2022-08-02 14:03:59 +0800

    test

--- lambda-matplotlib-image/build-and-push.sh
+++ lambda-matplotlib-image/build-and-push.sh
@@ -6,12 +6,13 @@ account=$(aws sts get-caller-identity --query Account --output text)
 region="ap-southeast-1"
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
 
-aws ecr describe-repositories --repository-names "${image}" --region ${region}  > /dev/null 2>&1
+#aws ecr describe-repositories --repository-names "${image}" --region ${region}  > /dev/null 2>&1
+aws ecr describe-repositories --repository-names "${image}" --region ${region} || aws ecr create-repository --repository-name "${image}" --region ${region}
 
-if [ $? -ne 0 ]
-then
-    aws ecr create-repository --repository-name "${image}"  --region ${region}  > /dev/null
-fi
+# if [ $? -ne 0 ]
+# then
+#     aws ecr create-repository --repository-name "${image}"  --region ${region}  > /dev/null
+# fi
 
 # Get the login command from ECR and execute it directly (for our own registry and for the base image registry)
 aws ecr get-login-password --region ${region} | sudo docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
@@ -26,4 +27,4 @@ echo "Pushing image to ECR ${fullname}"
 sudo docker push ${fullname}
 
 # Writing the image name to let the calling process extract it without manual intervention:
-echo "${fullname}" > ecr_image_fullname.txt
+# echo "${fullname}" > ecr_image_fullname.txt
